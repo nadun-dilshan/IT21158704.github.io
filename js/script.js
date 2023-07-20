1101707d68919c0e8c8bac90f96e601f8b819474
Nadun Dilshan <100205314+IT21158704@users.noreply.github.com>
2023-07-21 01:53:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,8 +35,7 @@ window.onscroll = () => {
 
 
 
-ScrollReveal({ 
-    // reset: true,
+ScrollReveal({
     distance: '80px',
     duration: 2000,
     delay: 200
@@ -56,3 +55,25 @@ const typed = new Typed('.multiple-text',{
     backDelay: 1000,
     loop: true
 })
+
+function toggleDarkMode() {
+    const icon = document.getElementById('toggleDark');
+    icon.classList.toggle('bxs-sun');
+    icon.classList.toggle('bxs-moon');
+
+    const root = document.documentElement;
+    if (icon.classList.contains('bxs-moon')) {
+        root.style.setProperty('--bg-color', '#d2d1e9');
+        root.style.setProperty('--second-bg-color', '#e2e1ec');
+        root.style.setProperty('--text-color', '#2f3133');
+        root.style.setProperty('--main-color', '#6246e9');
+    } else {
+        root.style.setProperty('--bg-color', '#2C3333');
+        root.style.setProperty('--second-bg-color', '#2E4F4F');
+        root.style.setProperty('--text-color', '#CBE4DE');
+        root.style.setProperty('--main-color', '#11aaaf');
+    }
+  }
+
+  const toggleIcon = document.getElementById('toggleDark');
+  toggleIcon.addEventListener('click', toggleDarkMode);
